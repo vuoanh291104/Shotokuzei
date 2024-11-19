6e9f887086ff7bb44bb81101fea7d5e233d932a7
HyuNie <huunien734@gmail.com>
2024-11-19 14:19:50 +0700

    + Add file user

---
+++ Shotokuzei/src/model/User.java
@@ -0,0 +1,40 @@
+package model;
+
+public class User {
+    private int uid;
+    private String uname;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int uid, String uname, String password) {
+        this.uid = uid;
+        this.uname = uname;
+        this.password = password;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
